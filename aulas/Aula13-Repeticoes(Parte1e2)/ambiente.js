63a27a4edaa6235a0dfb484a38e8e563cba59bc9
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2023-07-26 12:31:10 -0300

    created aula 13

---
+++ aulas/Aula13-Repeticoes(Parte1e2)/ambiente.js
@@ -0,0 +1,25 @@
+/* Estruturas de repetição */
+
+/* While */
+
+var cont = 1
+
+while (cont <= 5){
+    console.log(`While: ${cont}`)
+    cont++
+}
+
+/* Do While */
+
+var cont = 1
+
+do {
+    console.log(`Do/While: ${cont}`)
+    cont++
+} while (cont <= 5)
+
+/* For */
+
+for (var cont = 1; cont <= 5; cont++){
+    console.log(`For: ${cont}`)
+}
