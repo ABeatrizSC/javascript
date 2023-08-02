4d2658f867df0d2a70778b26ba899bfa5654000e
ABeatrizSC <105675205+ABeatrizSC@users.noreply.github.com>
2023-08-02 10:48:45 -0300

    update exercicio05

---
+++ exercicios/exercicio05/script.js
@@ -0,0 +1,14 @@
+function adicionar(){
+    var n = document.getElementById('txtnum')
+
+    var num = Number(n.value)
+
+    if (num > 100 || num <=0) {
+       window.alert("Por favor, digite números entre 1 e 100")
+    }
+
+}
+
+function finalizar (){
+    
+}
